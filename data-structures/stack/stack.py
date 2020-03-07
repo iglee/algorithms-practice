584c43c45dbba0518d0a991f613ca968b72b014b
Isabelle <lee.isabelle.g@gmail.com>
2020-03-07 14:32:30 -0800

    fixed stack

--- data-structures/stack/stack.py
+++ data-structures/stack/stack.py
@@ -13,12 +13,29 @@ class S:
         self.top = x
 
     def pop(self):
-        if self.stack:
-            if self.stack[-2]:
-                self.top = self.stack[-2]
-            else:
-                self.top = None
-            return  self.stack.pop()
+        if not self.empty():
+	    if len(self.stack)>1:
+		self.top = self.stack[-2]
+	    else:
+		self.top = None
+            return self.stack.pop()
+        return None
             
-        else:
-            return None
+
+if __name__ == "__main__":
+    s = S()
+    print("add element : ", 1)
+    s.push(1)
+    print("add element : ", 13)
+    s.push(13)
+    print("add element : ", 4)
+    s.push(4)
+    print("add element : ", 9)
+    s.push(9)
+    print("add element : ", 7)
+    s.push(7)
+
+    for i in range(5):
+        print("pop the latest element: ", s.pop())
+        print("current top: ", s.top)
+
